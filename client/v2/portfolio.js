8631c79bba55c68db5471f0353166818013ba574
ChevrollierG <chevrollier.guillaume@yahoo.com>
2022-01-29 18:27:33 +0100

    update client v2

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -116,6 +116,12 @@ selectShow.addEventListener('change', event => {
     .then(() => render(currentProducts, currentPagination));
 });
 
+selectPage.addEventListener('change', event => {
+  fetchProducts(parseInt(event.target.value), currentPagination.size)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination));
+});
+
 document.addEventListener('DOMContentLoaded', () =>
   fetchProducts()
     .then(setCurrentProducts)
