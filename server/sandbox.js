8ea57d6281f2dc0278398bfd337b3f8700d40caf
marie-donier <95496652+marie-donier@users.noreply.github.com>
2022-03-06 21:40:21 +0100

    week 3

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,21 +1,136 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
-  try {
+const fs = require("fs") //to store into a JSON file 
+
+
+async function sandbox(eshop, brand) {
+  try 
+  {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await brand.scrape(eshop);
 
     console.log(products);
     console.log('done');
-    process.exit(0);
-  } catch (e) {
+    return products;
+    //process.exit(0);
+  } 
+  
+  catch (e) 
+  {
     console.error(e);
-    process.exit(1);
+    //process.exit(1);
   }
 }
 
-const [,, eshop] = process.argv;
+function Scraping() {
+  ScrapingDedicated();
 
-sandbox(eshop);
+  ScrapingAdresseParis();
+
+  ScrapingMontlimart();
+}
+
+
+//Scraping Dedicated (with JSON API)
+function ScrapingDedicated() {
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  
+  var req = new XMLHttpRequest();
+  req.open('GET', 'https://www.dedicatedbrand.com/en/loadfilter', false);
+  req.send(null);
+
+  var json_file = JSON.parse(req.responseText)
+  var json_file_dedicated = []
+  for (document of json_file.products) 
+  {
+    if (document.uri != undefined) 
+    {
+      var product = {};
+      product.name = document.name;
+      product.price = parseInt(document.price.price);
+      product.image = document.image[0];
+      product.link = "https://www.dedicatedbrand.com/en/" + document.canonicalUri;
+      json_file_dedicated.push(product)
+    }
+  }
+
+  CreateJSONFile(json_file_dedicated, "./dedicated.json")
+}
+
+
+
+//Scraping Adresse Paris -> we store the products in two separated json files
+function ScrapingAdresseParis() {
+  const adresseparis = require('./sources/adresseparisbrand.js');
+  
+  var products_p1 = []
+  let link1 = "https://adresse.paris/630-toute-la-collection"
+  
+  products1 = sandbox(link1, adresseparis).then(products1 => {
+    for (var p of products1) 
+    {
+      products_p1.push(p)
+    }
+    
+    CreateJSONFile(products_p1, "./adresseparisp1.json")
+  })
+
+
+  var products_p2 = []
+  let link2 = "https://adresse.paris/630-toute-la-collection?p=2"
+  products2 = sandbox(link2, adresseparis).then(products2 => {
+    for (var p of products2) 
+    {
+      products_p2.push(p)
+    }
+
+    CreateJSONFile(products_p2, "./adresseparisp2.json")
+  })
+}
+
+
+
+//Scraping Montlimart
+function ScrapingMontlimart() {
+  const montlimart = require('./sources/montlimartbrand');
+  var all_products = []
+
+  // We get into each page (8 pages) with a loop
+  for (let i = 1; i <= 8; i++) 
+  {
+    let link = "https://www.montlimart.com/toute-la-collection.html" + "?p=" + i.toString()
+    
+    products = sandbox(link, montlimart).then(products => {
+      for (var p of products) 
+      {
+        all_products.push(p)
+      }
+
+      CreateJSONFile(all_products, "./montlimart.json")
+    })
+  }
+}
+
+
+
+// Function to store the list into a JSON file 
+function CreateJSONFile(products, path) {
+  console.log("enter JSON function");
+  products = JSON.stringify(products);
+  fs.writeFile(path, products, (err) => {
+      if (err) 
+      {
+        console.error(err);
+      } 
+      
+      else 
+      {
+        console.log('JSON file successfully written.');
+      }
+  });
+}
+
+
+
+Scraping();
--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -21,8 +21,11 @@ const parse = data => {
           .find('.productList-price')
           .text()
       );
+      let link = $(element).find('.productList-link').attr('href');
+        const prefix = 'https://www.dedicatedbrand.com';
+        link = prefix + link;
 
-      return {name, price};
+      return {name, price, link};
     })
     .get();
 };
@@ -32,6 +35,79 @@ const parse = data => {
  * @param  {[type]}  url
  * @return {Array|null}
  */
+module.exports.scrape = async url => {
+  try 
+  {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } 
+  
+  catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+
+
+
+
+
+
+
+/*
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+*/
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+
+/*
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.productList-container .productList')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.productList-title')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price = parseInt(
+        $(element)
+          .find('.productList-price')
+          .text()
+      );
+
+      return {name, price};
+    })
+    .get();
+};
+
+*/
+
+
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+
+/*
 module.exports.scrape = async url => {
   try {
     const response = await fetch(url);
@@ -50,3 +126,5 @@ module.exports.scrape = async url => {
     return null;
   }
 };
+
+*/
