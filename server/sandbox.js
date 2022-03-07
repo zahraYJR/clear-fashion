f58c3b859903cba8662f1c84487f07f43ecb35c6
ChevrollierG <chevrollier.guillaume@yahoo.com>
2022-03-07 11:28:58 +0100

    Td3 about scrapping data and saving it in JSON

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,12 +1,32 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const montlimart = require('./sources/Montlimart');
+const adresseParis = require('./sources/adresseParis');
+const fs = require('fs');
+const shop1='https://www.dedicatedbrand.com/en/men/all-men';
+const shop2='https://www.montlimart.com/toute-la-collection.html';
+const shop3='https://adresse.paris/630-toute-la-collection';
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop=shop2) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
-
+    const products = await montlimart.scrape(eshop);
+    for(let i=0;i<products.length;i++){
+      if(isNaN(products[i].price)){
+        products.splice(i,1);
+      }
+      else{
+        if(i!=products.length-1){
+          for(let j=i+1;j<products.length;j++){
+            if(products[i].name==products[j].name){
+              products.splice(j,1);
+            }
+          }
+        }
+      }
+    }
+    fs.writeFileSync('./sources/test.json',JSON.stringify(products,null,'\t'));
     console.log(products);
     console.log('done');
     process.exit(0);
@@ -16,6 +36,6 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   }
 }
 
-const [,, eshop] = process.argv;
+const [,,eshop] = process.argv;
 
 sandbox(eshop);
---
+++ server/sources/adresseParis.js
@@ -0,0 +1,52 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.center_column .product-container')
+    .map((i, element) => {
+      const name = $(element)
+        .find('.versionmob .product-name')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price = parseInt(
+        $(element)
+          .find('.right-block .content_price')
+          .text()
+      );
+
+      return {name, price};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
