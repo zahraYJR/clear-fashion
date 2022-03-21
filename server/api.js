1858f8749f2c082b4ac6fb453f963600a5fc7962
Artemis618 <artemis.coussin@edu.devinci.fr>
2022-03-21 22:45:38 +0100

    Api.js clean

--- server/api.js
+++ server/api.js
@@ -7,8 +7,6 @@ const MongoClient = require("mongodb").MongoClient;
 const mongodb = require("./mongodb");
 
 const PORT = 8092;
-const CONNECTION_URL = 'mongodb+srv://Artemis:ABEG19232202@fashion-sample.ea124.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DATABASE_NAME = "fashion-sample";
 const app = express();
 var database, collection;
 
@@ -98,33 +96,3 @@ app.get('/Liste_Products/:id', async (request,response)=> {
     console.log(result);
     response.send(result);
 });
-
-
-/*
-const Express = require("express");
-const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-
-const CONNECTION_URL = "mongodb+srv://Artemis:ABEG19232202@fashion-sample.ea124.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const DATABASE_NAME = "fashion-sample";
-const PORT = 8092;
-
-var app = Express();
-
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
-
-var database, collection;*/
-
-/*
-app.listen(PORT, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("people");
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
-});*/
