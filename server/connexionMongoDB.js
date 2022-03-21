995095a55bd0b4470e396e9f166ddd15297225fe
Leodespt <Leonard.desportes@edu.devinci.fr>
2022-03-21 16:13:58 +0100

    commit

--- server/connexionMongoDB.js
+++ server/connexionMongoDB.js
@@ -1,6 +1,7 @@
 const {MongoClient} = require('mongodb');
 const MONGODB_DB_NAME = 'clearfashion';
-const MONGODB_URI = "mongodb+srv://user:web_clear_fashion@clearfashion.biwwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_URI = "mongodb+srv://admin:mongoDB2@clearfashion.biwwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 
 async function connect() {
     try 
@@ -32,8 +33,6 @@ async function Insert_products(products)
     }
 }
 //Insert_products([all_products])
-
-
 
 // 5. Create at least 3 methods to find products according query
 
